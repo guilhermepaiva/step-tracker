465e12701ba90718e49d25b4b7b7eafbb45dd476
Guilherme Paiva <guilhermepaiva9@gmail.com>
2024-06-09 10:41:33 -0300

    Has seen permission priming and usage of AppStorage

--- Step Tracker/HealthkitPermissionPrimingView.swift
+++ Step Tracker/HealthkitPermissionPrimingView.swift
@@ -13,6 +13,7 @@ struct HealthkitPermissionPrimingView: View {
     @Environment(HealthKitManager.self) private var hkManager
     @Environment(\.dismiss) private var dismiss
     @State private var isShowingHealthKitPermissions = false
+    @Binding var hasSeen: Bool
     
     var description = """
     This app displays your step and weight data in interactive charts.
@@ -43,15 +44,17 @@ struct HealthkitPermissionPrimingView: View {
             .tint(.pink)
         }
         .padding(30)
+        .interactiveDismissDisabled()
+        .onAppear { hasSeen = true }
         .healthDataAccessRequest(
             store: hkManager.store,
             shareTypes: hkManager.types,
             readTypes: hkManager.types,
             trigger: isShowingHealthKitPermissions) { result in
                 switch result {
-                case .success(let success):
+                case .success(_):
                     dismiss()
-                case .failure(let failure):
+                case .failure(_):
                     // handle the error later
                     dismiss()
                 }
@@ -60,6 +63,6 @@ struct HealthkitPermissionPrimingView: View {
 }
 
 #Preview {
-    HealthkitPermissionPrimingView()
+    HealthkitPermissionPrimingView(hasSeen: .constant(true))
         .environment(HealthKitManager())
 }
